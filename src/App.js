5df0edca3ad7872e42cd8d3e2f558a0b4dae3edf
JJTTMOON <moon@JJTTMOONui-MacBook-Pro.local>
2017-06-20 14:16:40 +0900

    App.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>
+          Initial Setting
+        </Text>
+      </View>
+    );
+  }
+}
+
+export default App;
