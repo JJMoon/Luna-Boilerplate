eb825674a9b233c9d6654a2999260d55296ce5c6
JJTTMOON <moon@JJTTMOONui-MacBook-Pro.local>
2017-06-20 15:05:58 +0900

    Common

--- src/App.js
+++ src/App.js
@@ -1,3 +1,12 @@
+/**
+ * Bit Src Project / MediSimETT
+ * github.com/JJMoon/Luna-Boilerplate.git
+ *
+ * src / App.js
+ *
+ * Created by Jongwoo Moon on 2017. 6. 20..
+ */
+
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
